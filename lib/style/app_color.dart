caab8ad30858ed0f165c675d07dae0ecdd8eee2f
Rizky Agung Ramadhan <rizkyagungramadhan@gmail.com>
2022-05-17 21:04:55 +0700

    create AppColor & AppDimen class

---
+++ lib/style/app_color.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+/// Created by rizkyagungramadhan@gmail.com
+/// on 5/17/2022.
+
+class AppColor {
+  const AppColor._();
+
+  static const primary = Color(0xFF04A183);
+  static const accent = Color(0xff395766);
+
+  static const greyLight = Color(0xFFECECEC);
+  static const red = Color(0xFFFF1515);
+}
---
+++ lib/style/app_dimen.dart
@@ -0,0 +1,53 @@
+/// Created by rizkyagungramadhan@gmail.com 
+/// on 5/17/2022.
+
+
+class AppDimen {
+  const AppDimen._();
+
+  static const paddingSuperSmall = 2.0;
+  static const paddingExtraSmall = 4.0;
+  static const paddingSmall = 8.0;
+  static const paddingMedium = 12.0;
+  static const paddingLarge = 16.0;
+  static const paddingExtraLarge = 20.0;
+  static const paddingExtraLarge2 = 24.0;
+  static const paddingExtraLarge3 = 28.0;
+  static const paddingExtraLarge4 = 32.0;
+  static const paddingExtraLarge5 = 36.0;
+  static const paddingExtraLarge6 = 40.0;
+  static const paddingExtraLarge7 = 44.0;
+  static const paddingExtraLarge8 = 48.0;
+  static const paddingExtraLarge9 = 52.0;
+  static const paddingExtraLarge10 = 56.0;
+
+  static const fontExtraSmall = 4.0;
+  static const fontSmall = 8.0;
+  static const fontMedium = 12.0;
+  static const fontLarge = 16.0;
+  static const fontExtraLarge = 20.0;
+  static const fontExtraLarge2 = 24.0;
+  static const fontExtraLarge3 = 28.0;
+
+  static const buttonExtraSmall = 65.0;
+  static const buttonSmall = 100.0;
+
+  static const buttonMinimumSize = 50.0;
+  static const elevatedButtonMinimumHeight = 36.0;
+
+  static const iconSizeSmall = 8.0;
+  static const iconSizeMedium = 16.0;
+  static const iconSizeLarge = 20.0;
+  static const iconSizeExtraLarge = 24.0;
+  static const iconSizeExtraLarge2 = 28.0;
+  static const iconSizeExtraLarge3 = 32.0;
+  static const iconSizeMax = 48.0;
+  static const iconSizeMax2 = 56.0;
+
+  static const radiusExtraSmall = 4.0;
+  static const radiusSmall = 8.0;
+  static const radiusMedium = 12.0;
+  static const radiusLarge = 16.0;
+  static const radiusExtraLarge = 20.0;
+  static const radiusSuperLarge = 24.0;
+}
