2489ae5827f333ea70d2a0d44f61b58af5408542
Rizky Agung Ramadhan <rizkyagungramadhan@gmail.com>
2022-05-17 23:40:39 +0700

    remove dev purpose code & create real API call for music api

--- lib/api/music/music_api.dart
+++ lib/api/music/music_api.dart
@@ -16,16 +16,24 @@ class MusicApi {
 
   static const _searchAction = "/search";
 
-  /// Doc : Use it for development purpose only since the Search API is limited to approximately 20 calls per minute.
-  /// @author rizkyagungramadhan@gmail.com on 01-Apr-2022, Fri, 20:22.
   Future<ResponseList<MusicTrackResponse>> search(
       MusicTrackRequest request) async {
-    return await rootBundle
-        .loadString("assets/music_track_response.json")
-        .then((jsonString) {
-      var jsonMap = json.decode(jsonString);
-      return ResponseList<MusicTrackResponse>.fromJson(
-          jsonMap, (json) => MusicTrackResponse.fromJson(json));
-    });
+    return await _dioClient
+        .get(_searchAction, queryParameters: request.toJson())
+        .then((response) => ResponseList.fromJson(
+        response, (json) => MusicTrackResponse.fromJson(json)));
   }
+
+  /// Doc : Use it for development purpose only since the Search API is limited to approximately 20 calls per minute.
+  /// @author rizkyagungramadhan@gmail.com on 01-Apr-2022, Fri, 20:22.
+  // Future<ResponseList<MusicTrackResponse>> search(
+  //     MusicTrackRequest request) async {
+  //   return await rootBundle
+  //       .loadString("assets/music_track_response.json")
+  //       .then((jsonString) {
+  //     var jsonMap = json.decode(jsonString);
+  //     return ResponseList<MusicTrackResponse>.fromJson(
+  //         jsonMap, (json) => MusicTrackResponse.fromJson(json));
+  //   });
+  // }
 }
